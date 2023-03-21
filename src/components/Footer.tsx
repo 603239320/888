71d016f168b69b8a571e1d31045ebf767e8f8496
mxnf1314 <128242420+mxnf1314@users.noreply.github.com>
2023-03-21 09:31:42 +0800

    Update Footer.tsx
    Signed-off-by: mxnf1314 <128242420+mxnf1314@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default () => (
     <div class="flex" mt-3 gap-2>
       <a
         class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-amber-300 bg-amber px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:border-amber-400 hover:bg-amber-300 hover:text-slate-2"
-        href="https://www.buymeacoffee.com/yesmore/gallery"
+        href="http://mxnf.store/234.png"
         target="_blank"
         rel="noopener noreferrer">
         <svg
@@ -19,15 +19,15 @@ export default () => (
           xmlns="http://www.w3.org/2000/svg">
           <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path>
         </svg>
-        <span text-slate-800>欢迎打赏助力</span>
+        <span text-slate-800>账号购买</span>
       </a>
       <a
         class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-gray-300 bg-gray-3 px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:border-gray-500 hover:bg-gray-6 hover:text-slate-2"
-        href="https://github.com/yesmore/QA"
+        href="http://gpt.mxnf.store/123/123.jpg"
         target="_blank"
         rel="noopener noreferrer">
         <Github />
-        <span>Star on GitHub</span>
+        <span>搭建同款</span>
       </a>
     </div>
   </footer>
