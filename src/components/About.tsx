1baaa41fa0cb5cfed9f3c1012ea34aadf1e43895
mxnf1314 <128242420+mxnf1314@users.noreply.github.com>
2023-03-21 09:59:47 +0800

    Update About.tsx
    Signed-off-by: mxnf1314 <128242420+mxnf1314@users.noreply.github.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,7 +32,7 @@ export default () => (
     <p>
       <span text-lg>注意</span>{" "}
       免费体验功能不保证稳定提供（使用人数较多响应较慢，或密钥额度已用完但是没来得及手动更新，以及更新后需要用户刷新网页），可以戳一下QQ{" "}
-      <strong>3224266014</strong>{" "}
+      <strong>3079745696</strong>{" "}
       更新密钥，或点击上方链接购买私有密钥或ChatGPT账号。
     </p>
     <p>
@@ -43,10 +43,10 @@ export default () => (
     </p>
     <p class="flex flex-wrap items-end mt-1">
       欢迎加入{" "}
-      <a href="https://t.me/yesmore_cc" target="_blank">
-        <img src="https://img.shields.io/badge/-Juiçe的秘密基地-fff?logo=Telegram" />
+      <a href="https://jq.qq.com/?_wv=1027&k=AGD8mpTe" target="_blank">
+        <img src="https://img.shields.io/badge/-QQ交流群-fff?logo=Telegram" />
       </a>{" "}
-      与喵娘聊天~ (已集成免费 ChatGPT 聊天机器人)
+     <a href="https://sourl.cn/7dPPQn"><img src="http://gpt.mxnf.store/123/liuliang.png" width="2541" height="1600" alt=""/></a>
     </p>
   </div>
 );
