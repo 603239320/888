dc68ec8c116150cec94c2bcf5f477e3358f6bd2c
mxnf1314 <128242420+mxnf1314@users.noreply.github.com>
2023-03-22 23:21:00 +0800

    Update About.tsx
    Signed-off-by: mxnf1314 <128242420+mxnf1314@users.noreply.github.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -46,7 +46,7 @@ export default () => (
       <a href="http://mxnf.tk" target="_blank">
         <img src="https://img.shields.io/badge/-点我进入-fff?logo=Telegram" />
       </a>{" "}
-      </br>
+    
      <a href="https://sourl.cn/7dPPQn"><img src="http://mxnf.store/123/liuliang.png" width="2541" height="1600" alt=""/></a>
     </p>
   </div>
