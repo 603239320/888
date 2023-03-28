8377f5512b74814669e0858a61b0b7a270b43188
57676 <128841884+57676@users.noreply.github.com>
2023-03-28 17:36:30 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,6 +21,8 @@ export default () => (
         </svg>
         <span text-slate-800>账号购买</span>
          </a>
+      <a href="mqqapi://card/show_pslcard?src_type=internal&version=1&uin=群号码&card_type=group&source=qrcode">点击加入QQ群636104376
+      </a>
       <a
           class="flex max-w-fit items-center justify-center space-x-2 rounded-1 border border-gray-300 bg-gray-3 px-3 py-2 text-sm text-gray-600 shadow-sm transition-colors 
                  hover:border-gray-500 hover:bg-gray-6 hover:text-slate-2"
@@ -33,31 +35,3 @@ export default () => (
           </div>
   </footer>
 );
-import { useState } from "react";
-
-export default function Footer() {
-  const [redirectUrl, setRedirectUrl] = useState("");
-
-  const handleClick = () => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-      const qqUrl = "mqqapi://card/show_pslcard?src_type=internal&version=1&uin=636104376&card_type=group&source=qrcode";
-      setRedirectUrl(qqUrl);
-    } else {
-      const qqUrl = "https://jq.qq.com/?_wv=1027&k=KzLjRgD1";
-      window.open(qqUrl, "_blank");
-    }
-  };
-
-  if (redirectUrl) {
-    window.location.href = redirectUrl;
-  }
-
-  return (
-    <div className="flex items-center justify-center">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
-        加入QQ群
-      </button>
-    </div>
-  );
-}
