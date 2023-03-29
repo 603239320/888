3a22151c0a2458233a7620a958c4db6ed69c5a6d
57676 <128841884+57676@users.noreply.github.com>
2023-03-30 02:14:32 +0800

    Create check_password.php

---
+++ check_password.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>受保护的页面</title>
+</head>
+<body>
+	<h1>欢迎访问受保护的页面</h1>
+	<p>请输入密码以访问此页面:</p>
+	<form action="protected.html" method="post">
+		<input type="password" name="password">
+		<input type="submit" value="提交">
+	</form>
+</body>
+</html>
