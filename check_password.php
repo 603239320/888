14dd222f09665d1000bc16109a647a5909b50c86
57676 <128841884+57676@users.noreply.github.com>
2023-03-30 02:22:02 +0800

    Update check_password.php

--- check_password.php
+++ check_password.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>受保护的页面</title>
+	<title>Protected Page</title>
 </head>
 <body>
-	<h1>欢迎访问受保护的页面</h1>
-	<p>请输入密码以访问此页面:</p>
-	<form action="protected.html" method="post">
+	<?php
+		$password = "mypassword"; // 设置密码
+		if(isset($_POST['password'])) {
+			if($_POST['password'] == $password) {
+				header('Location: protected.html'); // 密码正确，跳转到受保护的页面
+			} else {
+				echo "<p>Incorrect password. Please try again.</p>"; // 密码错误，显示错误信息
+			}
+		}
+	?>
+	<h1>Welcome to the Protected Page</h1>
+	<p>Please enter the password to access this page:</p>
+	<form action="" method="post">
 		<input type="password" name="password">
-		<input type="submit" value="提交">
+		<input type="submit" value="Submit">
 	</form>
 </body>
 </html>
