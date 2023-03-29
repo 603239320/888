5a0f6423da42e355957e509d3778ccb9aace49a1
57676 <128841884+57676@users.noreply.github.com>
2023-03-30 02:41:21 +0800

    Update checkpassword.php

--- checkpassword.php
+++ checkpassword.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Protected Page</title>
+	<title>受保护的页面</title>
 </head>
 <body>
-	<h1>Welcome to my protected page</h1>
-	<p>Please enter the password to proceed</p>
+	<h1>欢迎来到我的受保护的页面</h1>
+	<p>请输入密码以继续</p>
 	<form action="checkpassword.php" method="post">
-		<label for="password">Password:</label>
+		<label for="password">密码：</label>
 		<input type="password" id="password" name="password">
-		<input type="submit" value="Submit">
+		<input type="submit" value="提交">
 	</form>
 </body>
 </html>
- 
