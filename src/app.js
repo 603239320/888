8e5bf906d1daf07a834b06343e3fca420c830e19
57676 <128841884+57676@users.noreply.github.com>
2023-03-30 03:18:22 +0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const password = process.env.PASSWORD;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  const auth = {login: 'admin', password: password}; // 定义用户名和密码
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, pwd] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  // 验证用户名和密码是否正确
+  if (login && pwd && login === auth.login && pwd === auth.password) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  } else {
+    res.set('WWW-Authenticate', 'Basic realm="401"');
+    res.status(401).send('Authentication required.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
