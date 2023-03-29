41f5ee883b0701a658dbee43f971d2718358150d
57676 <128841884+57676@users.noreply.github.com>
2023-03-30 02:25:56 +0800

    Update and rename check_password.php to checkpassword.php

---
+++ checkpassword.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Protected Page</title>
+</head>
+<body>
+	<h1>Welcome to my protected page</h1>
+	<p>Please enter the password to proceed</p>
+	<form action="checkpassword.php" method="post">
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
